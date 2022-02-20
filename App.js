2e5fa07dda101305a8a137f14b42926f0240577a
Balasubramaniyam Kumar <balasubramaniyamk1995@gmail.com>
2022-02-20 19:09:14 +0530

    restaurant card component added

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+
+import { RestaurantsScreen } from "./src/features/restaurants/screen/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: StatusBar.currentHeight },
-  search: { padding: 16 },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" },
-});
