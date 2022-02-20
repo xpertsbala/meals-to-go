8d1a5f3533bfd42f8ef1fe035cc6861a25a2e66f
Balasubramaniyam Kumar <balasubramaniyamk1995@gmail.com>
2022-02-20 20:01:53 +0530

    infra themes js files added, theme provider logic implemented

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screen/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
     </>
   );
 }
